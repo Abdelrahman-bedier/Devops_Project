d5de479520d447ca8b74621eecd0c0b0ced36d7a
Abdelrahman-bedier <abdo.bedier@gmail.com>
2024-10-16 11:26:54 +0300

    create database in docker compose

--- db-init-scripts/database_commands.sql
+++ db-init-scripts/database_commands.sql
@@ -1,5 +1,5 @@
-create database postgres_db;
-\c postgres_db;
+-- create database postgres_db;
+-- \c postgres_db;
 
 CREATE TABLE IF NOT EXISTS posts (
     id SERIAL PRIMARY KEY,
@@ -8,10 +8,10 @@ CREATE TABLE IF NOT EXISTS posts (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO posts (title, body) VALUES ('First Post', 'This is my first post'), ('Second Post', 'This is my second post'), ('Third Post', 'Here is my third post');
+INSERT INTO posts (title, body) VALUES ('First Post', 'This is my first post'), ('Second Post', 'This is my second post'), ('Third Post', 'Here is my third post!');
 
 /*
 after docker compose connect to the database by using this command and write password 'password'
  psql -h 0.0.0.0 -p 5432 -d postgres_db -U postgres -W
  */
- 
+--  ALTER USER postgres with encrypted password 'password';
